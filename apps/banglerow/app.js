34e0c75be5855dafa5669ccdcda0818bfc4f4a3b
maurosbicego <mauro@sbicego.ch>
2020-06-21 21:33:16 +0200

    Now using different method to count strokes

--- apps/banglerow/app.js
+++ apps/banglerow/app.js
@@ -1,73 +1,31 @@
-var high = 0
-var low = 0
-var highdiff
-var lowdiff
-var state
-var measurements = []
-var laststatechange = Date.now()
-var lastnotified = Date.now()
-var pacetime
-var sum
-
-function stateChange(newstate) {
-  if (((Date.now() - laststatechange) / 1000) > 1) {
-    state = newstate
-    elapsed = Date.now() - laststatechange
-    measurements.push(elapsed)
-    measurements = measurements.slice(Math.max(measurements.length - 6, 0))
-    sum = 0
-    for( var i = 0; i < measurements.length; i++ ){
-      sum += parseInt( measurements[i] );
-  }
-    pacetime = (sum / measurements.length) / 1000
-    pace = Math.round(60 / pacetime)
-    binaryrev = pace.toString(2).split("").reverse().join("")
-    laststatechange = Date.now()
-    E.showMessage("Pace",pace)
-    if (Date.now() - lastnotified > 10000000) {
-      binaryrev.split("").forEach(function(val) {
-        if (val == "0") {
-          Bangle.buzz(100)
-        } else {
-          Bangle.buzz(300)
-        }
-
-      })
-
-      lastnotified = Date.now()
-    }
-  }
-}
+var samples = []
+var strokes = []
+var stroke = 0
+var strokeamount = 5
+var samplerate = 12 // Samplerate = ~12 SPS
+var laststroke = new Date(Date.now() - 1000);
+var startdate = Date.now()
 
 function measure() {
-  if (high === 0 || low === 0) {
-    Bangle.beep(500, 600)
-  } else {
-    state = 0
-    Bangle.on('accel', function(accel) {
-      highdiff =  (high.x - accel.x) + (high.y - accel.y) + (high.z - accel.z)
-      lowdiff = (low.x - accel.x) + (low.y - accel.y) + (low.z - accel.z)
-      if (Math.abs(highdiff) < Math.abs(lowdiff)) {
-        if (state !== 1) {
-          stateChange(1)
+  Bangle.on('accel', function(accel) {
+    samples.push(accel)
+    if (samples.length > 100) {
+      samples.shift()
+    }
+    if (Date.now() - startdate > 2000) {
+      if ((accel.y < accel.x) && (accel.x < samples[samples.length - 2].x) && (samples[samples.length - 2].x > samples[samples.length - 4].x) && (samples[samples.length - 4].x < accel.x) && (samples[samples.length - 4].y > accel.y) && (Date.now() - laststroke > 1000)) {
+        Bangle.buzz()
+        laststroke = Date.now()
+        strokes.push(Date.now())
+        if (strokes.length > strokeamount) {
+          strokes.shift()
         }
-      } else {
-        if (state !== 0) {
-          stateChange(0)
-        }
+        stroke = 60/(((strokes[strokes.length-1] - strokes[0])/1000)/strokeamount)
+        E.showMessage("Stroke", stroke)
       }
-
-    });
-  }
+    }
+  })
 }
 
-function setLow() {
-  low = Bangle.getAccel()
-  measure()
-}
-function setHigh() {
-  high = Bangle.getAccel()
-  measure()
-}
-setWatch(setHigh, BTN1, { repeat: true });
-setWatch(setLow, BTN3, { repeat: true });
+
+measure()
